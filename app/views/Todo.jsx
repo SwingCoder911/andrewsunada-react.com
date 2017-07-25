6514678775fa0f8534ab8f83d8fee479c6ed15f1
Andrew Sunada <kaleb2azriel@gmail.com>
2017-07-25 13:24:57 -0700

    Getting started with todo actions

--- app/views/Todo.jsx
+++ app/views/Todo.jsx
@@ -6,11 +6,23 @@ export class Todo extends Component {
   constructor(){
     super();
   }
+  addItem(input){
+    console.log("clicked: ", input);
+  }
   render() {
+    var input;
     return (
-      <div className="todo">
-        <h1>Todo App</h1>
-        <Button>Click Me!</Button>
+      <div className="todo container">
+        <div className="col-md-8 col-md-offset-2">
+          <h1>Todo App</h1>
+          <input ref={node => {
+            input = node;
+          }}/>
+          <Button onClick={() => {
+            this.addItem(input.value);
+            input.value = "";
+          }}>Click Me!</Button>
+        </div>
       </div>
     );
   }
